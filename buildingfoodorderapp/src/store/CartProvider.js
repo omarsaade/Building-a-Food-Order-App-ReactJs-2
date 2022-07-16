3b83c4a54e1cd302f254ef3d74c5f52de26cd83e
omar saade <omarsaadeh11@gmail.com>
2022-07-16 13:09:40 +0300

    react

--- buildingfoodorderapp/src/store/CartProvider.js
+++ buildingfoodorderapp/src/store/CartProvider.js
@@ -9,22 +9,52 @@ const defaultCartState = {
 
 
 
-//reducer function
-//(prevstate,action)
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
-        const updatedItems = state.items.concat(action.item);//[{a}]
-        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount; // 0 + 22 * 2 = 44
+
+
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount; // 0 +22 * 1 = 22
+
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id // -1 bcz no match found  m1=m1
+        );
+
+
+        const existingCartItem = state.items[existingCartItemIndex]; //undefined / {id..}
+        let updatedItems;//undefiend
+
+        //Condition
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount //2
+            };
+            updatedItems = [...state.items];
+            // console.log(updatedItems);   [{…}]
+            updatedItems[existingCartItemIndex] = updatedItem;
+            // console.log(updatedItems[existingCartItemIndex]); { id: 'm1', name: 'Sushi', amount: 2, price: 22.99}
+            // console.log(updatedItem); {id: 'm1', name: 'Sushi', amount: 2, price: 22.99}
+        }
+        else {
+            updatedItems = state.items.concat(action.item) //[{a}]
+        }
+
+
         return {
             items: updatedItems, //[{a}]
             totalAmount: updatedTotalAmount // 44
         };
+
     }
+
     return defaultCartState; //fadye
 }
 
 // useReducer is usually preferable to useState when you have complex state 
 // logic that involves multiple sub - values or when the next state depends on the previous one.
+
+
+
 
 function CartProvider(props) {
 
