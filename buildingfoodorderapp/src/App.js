1e36c72dc911fe6d0e78dba8c23031901a391c0b
omar saade <omarsaadeh11@gmail.com>
2022-08-12 18:16:29 +0300

    react

--- buildingfoodorderapp/src/App.js
+++ buildingfoodorderapp/src/App.js
@@ -5,6 +5,7 @@ import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 import CartProvider from './store/CartProvider';
 
+
 function App() {
 
   const [cartIsShown, setCartIsShown] = useState(false);
